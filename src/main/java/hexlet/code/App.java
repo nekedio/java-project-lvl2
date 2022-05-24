083fcebd779f2836bb8ea9b205282cf7cef5bfa9
nekedio <nikitasi@tatmedia.com>
2022-05-24 14:50:35 +0300

    changed the help message

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 import java.io.File;
@@ -11,8 +12,14 @@ import java.util.concurrent.Callable;
         description = "Compares two configuration files and shows a difference.")
     class App implements Callable<Integer> {
 
-    @Parameters(index = "0", description = "The file whose checksum to calculate.")
-    private File file;
+    @Parameters(index = "0", description = "path to first file")
+    private File filepath1 ;
+
+    @Parameters(index = "1", description = "path to second file")
+    private File filepath2 ;
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format = "stylish";
 
     @Override
     public Integer call() throws Exception {
